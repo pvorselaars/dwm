023600da1d9fed9ca1ab16f38b6d87c8cc2f9b41
Perry Vorselaars <perry@perigee.xyz>
2021-01-11 20:05:59 +0100

    Changed default colours.

--- config.def.h
+++ config.def.h
@@ -14,8 +14,8 @@ static char normbgcolor[]           = "#222222";
 static char normbordercolor[]       = "#444444";
 static char normfgcolor[]           = "#bbbbbb";
 static char selfgcolor[]            = "#eeeeee";
-static char selbordercolor[]        = "#005577";
-static char selbgcolor[]            = "#005577";
+static char selbordercolor[]        = "#333333";
+static char selbgcolor[]            = "#222222";
 static char *colors[][3] = {
        /*               fg           bg           border   */
        [SchemeNorm] = { normfgcolor, normbgcolor, normbordercolor },
