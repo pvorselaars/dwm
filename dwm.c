6bac4249a6c9c994eff3f9b75da459779baed01c
Perry Vorselaars <perry@perigee.xyz>
2021-12-10 03:09:01 +0100

    Added autostart function
    Runs ~/.config/dwm/autostart.sh

--- dwm.c
+++ dwm.c
@@ -212,6 +212,7 @@ static void resizeclient(Client *c, int x, int y, int w, int h);
 static void resizemouse(const Arg *arg);
 static void restack(Monitor *m);
 static void run(void);
+static void autostart(void);
 static void scan(void);
 static int sendevent(Client *c, Atom proto);
 static void sendmon(Client *c, Monitor *m);
@@ -1434,6 +1435,12 @@ run(void)
 }
 
 void
+autostart(void)
+{
+	system("cd $HOME/.config/dwm; ./autostart.sh");
+}
+
+void
 scan(void)
 {
 	unsigned int i, num;
@@ -2219,6 +2226,7 @@ main(int argc, char *argv[])
 		die("pledge");
 #endif /* __OpenBSD__ */
 	scan();
+	autostart();
 	run();
 	cleanup();
 	XCloseDisplay(dpy);
